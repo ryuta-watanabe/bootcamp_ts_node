c1fa32f87eade6bc97c9e7f387f34273c27e1f4b
ryuta-watanabe <atelier.hunt777@gmail.com>
2023-04-09 17:40:51 +0900

    HTTPサーバを実装してみようの対応

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,48 @@
+import * as fs from "node:fs";
 import http from "node:http";
+import path from "path";
 const server = http.createServer();
 
+type mimeTypes = {
+  [key: string]: string;
+};
+
 server.on("request", async (req, res) => {
-  console.log("request url: ", req.url);
-  // Content-Type is important for browsers.
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.write("hello!\n");
-  res.end();
+  let filePath = `./public${req.url}`;
+  if (filePath === "/") filePath = "./index.html";
+  console.log("filePath: ", filePath);
+
+  const extname: string = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes: mimeTypes = {
+    ".html": "text/html",
+    ".json": "text/json",
+    ".jpg": "image/jpeg",
+    ".ico": "image/x-icon",
+  };
+  const contentType: string = mimeTypes[extname] || "application/octet-stream";
+
+  fs.readFile(filePath, (error, content) => {
+    console.log("content: ", content);
+    if (error) {
+      if (error.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.write("404 Not Found");
+        res.end();
+      } else {
+        res.writeHead(500);
+        res.end(
+          `Sorry, check with the site admin for error: ${error.code} ..\n`
+        );
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
 });
 
-server.on("listening", () => {
-  console.log("start listening!");
+server.listen(process.env.PORT || 12345, () => {
+  console.log("listen!");
 });
 
-// Start listening 12345 port of localhost (127.0.0.1).
-server.listen(12345, () => {
-  console.log("listening on http://localhost:12345/");
-});
 console.log("run server.js");
